6a2a04bc16c4602d02d6f072036141e086262258
asilbek <ak.fayzullayev@gmail.com>
2025-06-05 13:59:11 +0500

    Product

--- src/pages/product/Products.js
+++ src/pages/product/Products.js
@@ -7,59 +7,60 @@ const Products = () => {
   const navigate = useNavigate();
 
   return (
-    <div
-      style={{
-        padding: "40px",
-        display: "flex",
-        justifyContent: "center",
-        backgroundColor: "#f2eadc",
-      }}
-    >
-      <Row gutter={[80, 24]} justify="center" style={{ maxWidth: "1500px" }}>
-        {fruitsData.products.map((fruit, index) => (
-          <Col key={index}>
-            <Card
-              hoverable
-              className="product-card"
-              cover={
-                <div
-                  style={{ position: "relative", width: "100%", height: 500 }}
-                >
-                  <img
-                    src={fruit.photo[0]}
-                    alt={fruit.name}
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      objectFit: "cover",
-                    }}
-                  />
-
-                  <div className="product-name-overlay">{fruit.name}</div>
-
-                  {fruit.frame && (
-                    <img
-                      src={fruit.frame}
-                      alt={`${fruit.name} frame`}
-                      className="zoom-in"
+    <div style={{ backgroundColor: "#f2eadc" }}>
+      <Row justify="center">
+        <Col sm={24} md={24} lg={24} xl={24} xxl={18}>
+          <Row gutter={[80, 24]} className="mt-5 mb-5" justify="center">
+            {fruitsData.products.map((fruit, index) => (
+              <Col key={index}>
+                <Card
+                  hoverable
+                  className="product-card"
+                  cover={
+                    <div
                       style={{
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
+                        position: "relative",
                         width: "100%",
-                        height: "100%",
-                        pointerEvents: "none",
+                        height: 500,
                       }}
-                    />
-                  )}
-                </div>
-              }
-              onClick={() => {
-                navigate(`/products/${fruit.id}`);
-              }}
-            ></Card>
-          </Col>
-        ))}
+                    >
+                      <img
+                        src={fruit.photo[0]}
+                        alt={fruit.name}
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                        }}
+                      />
+
+                      <div className="product-name-overlay">{fruit.name}</div>
+
+                      {fruit.frame && (
+                        <img
+                          src={fruit.frame}
+                          alt={`${fruit.name} frame`}
+                          className="zoom-in"
+                          style={{
+                            position: "absolute",
+                            top: 0,
+                            left: 0,
+                            width: "100%",
+                            height: "100%",
+                            pointerEvents: "none",
+                          }}
+                        />
+                      )}
+                    </div>
+                  }
+                  onClick={() => {
+                    navigate(`/products/${fruit.id}`);
+                  }}
+                ></Card>
+              </Col>
+            ))}
+          </Row>
+        </Col>
       </Row>
     </div>
   );
